d93b78c835cb2d97a4cccf22197e9d2b113fe414
Mike <mikeblemberg@gmail.com>
2020-02-20 22:22:01 -0600

    update

--- currentConnections.m
+++ currentConnections.m
@@ -1,6 +1,23 @@
-function currentConnections = currentConnections(varargin)
+function ret = currentConnections(varargin)
   
-  size = varargin{1};   %[nX nY]
+  xNodes = varargin{1}(1);
+  yNodes = varargin{1}(2);
+  
+  type = varargin{2};
+  
+  ret = zeros(yNodes, xNodes);
+  
+  switch type
+    case 'ends'
+      ret(:,1) = 1;
+      ret(:,yNodes) = -1;
+  case 'points'
+      
+    
+    
+  endswitch
+      
+  
   
   
 endfunction
