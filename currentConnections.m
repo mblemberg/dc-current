828fc446cf466043323611f0aa2a4fcbf768f89e
MICHAEL BLEMBERG <mike@michaels-mbp.lan>
2020-02-20 23:09:22 -0600

    quick

--- currentConnections.m
+++ currentConnections.m
@@ -10,8 +10,11 @@ function ret = currentConnections(varargin)
   
   switch connectionType
     case 'ends'
-      ret(:,1) = 1;
-      ret(:,yNodes) = -1;
+      %First additional argument is 1 if the left hand side should be set, 0 otherwise
+      %Second additional argument is 1 if the right hand side should be set, 0 otherwise
+      if (varargin{4}); ret(:,1) = 1; endif;
+      if (varargin{4}); ret(:,yNodes) = -1; endif; 
+
     case 'points'
       %Each additional argument is of the form [x y I]
       for(i = 4:nargin) {
@@ -19,12 +22,19 @@ function ret = currentConnections(varargin)
       }
     case 'circles'
       %Each additional argument is of the form [x y r I]
-      
+      for i = 1:yNodes
+        for j = 1:xNodes
+          for k = 4:nargin
+            if ((j-varargin{k}(1)/meshSize-1)^2 + (i-varargin{k}(2)/meshSize-1)^2 <= (varargin{k}(3)/meshSize)^2)
+              currentConnections(i,j) = varargin{k}(4);
+            endif
+          endfor
+        endfor
+      endfor
+    case 'csv'
+      %Argument is the filename
+      currentConnections = csvread(varargin{4});
 
-
-      
-    
-    
   endswitch
       
   
